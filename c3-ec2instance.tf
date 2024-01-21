ffefd3bf0d0a5de3ab39ebb42205d2049c0fe440
Ojeranti08 <kemiola190@gmail.com>
2024-01-21 06:49:04 -0500

    file modified

---
+++ c3-ec2instance.tf
@@ -0,0 +1,11 @@
+/*
+# EC2 Instance Resources
+resource "aws_instance" "ecomm" {
+  ami           = "ami-0005e0cfe09cc9050"
+  instance_type = "t2.micro"
+  key_name      = "Oje"
+  tags = {
+    Name = "Ecomm"
+  }
+}
+*/
