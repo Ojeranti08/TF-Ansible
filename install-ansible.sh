d7df97aca15fad80ad007ce51e45321dc490546e
Oluwakemi Ojeranti <kemiola190@gmail.com>
2024-02-08 16:41:32 -0500

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum -y install epel-release
-sudo yum -y install ansible
 sudo yum -y install git
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-ansible -m ping node1 aws.ini 
-mkdir ansible-repo 
-cd ansible-repo
 git clone https://github.com/CeeyIT-Solutions/ecomm-3.git
+cd ecomm-3
 sudo mv ecomm-3/* /var/www/html/ 
