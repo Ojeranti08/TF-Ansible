fa409cfb772393675c58e27889f3937115ee2de1
Oluwakemi Ojeranti <kemiola190@gmail.com>
2024-02-08 16:43:18 -0500

    Update c4-ansible.tf

--- c4-ansible.tf
+++ c4-ansible.tf
@@ -1,5 +1,5 @@
 # Creating Ansible Master EC2 Resources
-resource "aws_instance" "ansible_master" {
+resource "aws_instance" "Ecomm" {
   ami                    = "ami-0005e0cfe09cc9050"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.ecomm-public-subnet.id
@@ -9,20 +9,6 @@ resource "aws_instance" "ansible_master" {
   private_ip             = "10.0.1.10"
 
   tags = {
-    Name = "Ansible Master"
-  }
-}
-
-# Creating Ansible Node EC2 Resources
-resource "aws_instance" "ansible_node1" {
-  ami                    = "ami-0005e0cfe09cc9050"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.ecomm-public-subnet.id
-  key_name               = "Oje"
-  vpc_security_group_ids = [aws_security_group.ecomm-sg.id]
-  user_data              = file("install-ansible.sh")
-  private_ip = "10.0.1.12"
-  tags = {
-    Name = "Ansible Node1"
+    Name = "Ecomm"
   }
 }
